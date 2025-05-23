0177b7da16f9f74a25750a977cae75bfc968402e
P-Lidia <Lydiasmolyakova@gmail.com>
2025-05-23 15:04:27 +0200

    Homework week 20. HTTP-methods, CRUD

--- src/main/java/ru/itgirl/libraryProject/model/entity/Book.java
+++ src/main/java/ru/itgirl/libraryProject/model/entity/Book.java
@@ -1,7 +1,20 @@
 package ru.itgirl.libraryProject.model.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Set;
 
@@ -22,6 +35,7 @@ public class Book {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "genre_id")
+    @Setter
     private Genre genre;
 
     @ManyToMany(fetch = FetchType.LAZY)
--- src/main/java/ru/itgirl/libraryProject/model/service/BookService.java
+++ src/main/java/ru/itgirl/libraryProject/model/service/BookService.java
@@ -1,8 +1,5 @@
 package ru.itgirl.libraryProject.model.service;
 
-import ru.itgirl.libraryProject.model.dto.AuthorCreateDto;
-import ru.itgirl.libraryProject.model.dto.AuthorDto;
-import ru.itgirl.libraryProject.model.dto.AuthorUpdateDto;
 import ru.itgirl.libraryProject.model.dto.BookCreateDto;
 import ru.itgirl.libraryProject.model.dto.BookDto;
 import ru.itgirl.libraryProject.model.dto.BookUpdateDto;
